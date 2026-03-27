888f4dcee7a2829f0fa2dae3cd8e10e1b91d6210
ikeda_takumi <zhuomichitisn03@gmail.com>
2026-03-27 16:31:18 +0900

    ボス実装

--- project/game/enemy/Boss/BossEnemy.h
+++ project/game/enemy/Boss/BossEnemy.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "BaseScene.h"
 #include "Camera.h"
 #include "CameraManager.h"
@@ -12,42 +12,55 @@
 #include "SoundManager.h"
 #include "Sprite.h"
 
-#include "../Normal/EnemyBullet.h"
+#include "../EnemyBullet.h"
 
 class Player;
+class PlayerBullet;
+
+struct CollisionVolume {
+    Vector3 position;
+    float radius;
+    uint32_t partId;
+};
 
 class BossEnemy {
 public:
     /// <summary>
-    /// ������
+    /// 初期化
     /// </summary>
     /// <param name="camera"></param>
     virtual void Initialize(Camera* camera, Vector3 pos, int health);
 
     /// <summary>
-    /// �X�V
+    /// 更新
     /// </summary>
     virtual void Update();
 
     /// <summary>
-    /// �`��
+    /// 描画
     /// </summary>
     virtual void Draw3D();
 
-    /* Get�֐� */
+    /* Get関数 */
     virtual Vector3 GetWorldPosition() const = 0;
     virtual float GetRadius() const = 0;
     const std::vector<std::unique_ptr<EnemyBullet>>& GetBullets() const { return enemyBullet_; }
+    // --- 共通の当たり判定インターフェース ---
+    virtual std::vector<CollisionVolume> GetCollisionVolumes() = 0;
+
+    // 弾を受け取り、弾を消滅させるなら true を返す
+    virtual bool OnHit(const CollisionVolume& volume, PlayerBullet* bullet) = 0;
+
     virtual bool GetIsDead() const = 0;
 
-    /* Set�֐� */
+    /* Set関数 */
     virtual void SetTargetPlayer(Player* target) { };
     virtual void OnCollision(int Damage, [[maybe_unused]] Vector3 bulletPos, [[maybe_unused]] Vector3 Velocity) = 0;
     // virtual void SetWayPoints(const std::vector<WayPoint>& waypoints) { };
     // virtual void SetFleeWaypoint(const WayPoint& fleeWP, bool hasFleeData) { };
 
 protected:
-    // �e
+    // 弾
     std::vector<std::unique_ptr<EnemyBullet>> enemyBullet_;
 
 private:
