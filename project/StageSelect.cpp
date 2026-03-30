f81a3af05305a7c8c6e7e1af5aec508d27cc54c7
オノゲンキ <k024G0005@g.neec.ac.jp>
2026-03-30 11:08:57 +0900

    ステージセレクト中

---
+++ project/StageSelect.cpp
@@ -0,0 +1,108 @@
+﻿#include "StageSelect.h"
+#include "ObjectCommon.h"
+#include "SceneManager.h"
+#include "SpriteCommon.h"
+
+
+#pragma comment(lib, "Dbghelp.lib")
+#pragma comment(lib, "dxcompiler.lib")
+
+void StageSelect::Initialize()
+{
+	// カメラ初期化
+	camera = std::make_unique<Camera>();
+	camera->SetRotate({ cameraTransform.rotate });
+	camera->SetTranslate({ cameraTransform.translate });
+
+	// カメラマネージャ登録
+	CameraManager::GetInstance()->AddCamera("main", camera.get());
+	CameraManager::GetInstance()->SetActiveCamera("main");
+
+	// スプライト
+	sprite = std::make_unique <Sprite>();
+	sprite->Initialize("Resource/monsterBall.png");
+
+	// 3Dオブジェクト
+	for (int i = 0; i < 2; i++) {
+		object[i] = std::make_unique <Object>();
+		object[i]->Initialize(camera.get());
+	}
+
+	// 初期化済みの3Dオブジェクトにモデルを紐づける
+	object[0]->SetModel("emission.obj");
+	object[1]->SetModel("skydome.obj");
+
+	currentStyle = style;
+
+	playerObject_ = std::make_unique<Object>();
+	playerObject_->Initialize(camera_);
+	switch (style) {
+	case Player::normal:
+		playerObject_->SetModel("normalMachine.obj");
+		break;
+	case Player::speed:
+		playerObject_->SetModel("speedMachine.obj");
+		break;
+	case Player::power:
+		playerObject_->SetModel("powerMachine.obj");
+		break;
+	case Player::sniper:
+		playerObject_->SetModel("sniperMachine.obj");
+		break;
+	default:
+		playerObject_->SetModel("normalMachine.obj");
+		break;
+	}
+
+	playerObject_->SetTranslate(transform_.translate);
+
+}
+
+void StageSelect::Update()
+{
+	// 入力取得
+	auto input = Input::GetInstance();
+	// カメラ更新
+	CameraManager::GetInstance()->Update();
+
+
+
+	// ENTERキーを押したら
+	if (input->TriggerKey(DIK_RETURN)) {
+		// ゲームプレイシーン(次シーン)を生成
+		SceneManager::GetInstance()->ChangeScene("GAMEPLAY");
+		// 音声再生
+		SoundManager::GetInstance()->Stop("bgm");
+	}
+
+	// * 3Dオブジェクト* //
+	for (int i = 0; i < 2; i++) {
+		object[i]->Update();
+	}
+
+	sprite->Update();
+
+}
+void StageSelect::Draw2D()
+{
+	// 2Dオブジェクトの描画準備
+	SpriteCommon::GetInstance()->SetCommonPipelineState();
+}
+
+void StageSelect::Draw3D()
+{
+	// 3Dオブジェクトの描画準備
+	ObjectCommon::GetInstance()->SetCommonPipelineState();
+
+
+	// 3Dオブジェクト描画
+	for (int i = 0; i < 2; i++) {
+		object[i]->Draw();
+	}
+
+}
+
+void StageSelect::Finalize()
+{
+	CameraManager::GetInstance()->RemoveCamera("main");
+}
---
+++ project/StageSelect.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include <DirectXMath.h>
+
+#include "Camera.h"
+#include "CameraManager.h"
+#include "Sprite.h"
+#include "Object.h"
+#include "ParticleEmitter.h"
+#include "CameraManager.h"
+#include "ParticleManager.h"
+#include "ModelManager.h"
+#include "SoundManager.h"
+#include "Input.h"
+#include "ImGuiManager.h"
+#include "BaseScene.h"
+#include "PostEffect.h"
+#include "RailCamera.h"
+
+using namespace DirectX;
+
+class SpriteCommon;
+class ObjectCommon;
+
+class StageSelect : public BaseScene
+{
+public:
+
+	//乗り物
+	enum Style {
+		normal,
+		speed,
+		power,
+		sniper
+	};
+
+	void Initialize()override;
+	void Update()override;
+	void Draw2D()override;
+	void Draw3D()override;
+
+	void Finalize()override;
+
+private:
+
+	// プレイヤーの3Dオブジェクト
+	std::unique_ptr<Object> playerObject_;
+
+	Transform cameraTransform{
+	   { 1.0f, 1.0f, 1.0f }, // scale
+	   { 0.0f, 0.0f, 0.0f }, // rotate
+	   { 0.0f, 0.0f, -5.0f } // translate
+	};
+	// パーティクル
+	Transform transformParticle
+	{
+		{1.0f,1.0f,1.0f},
+		{0.0f,0.0f,0.0f},
+		{0.0f,0.0f,0.0f}
+	};
+
+	// カメラ
+	std::unique_ptr<Camera> camera = nullptr;
+	// スプライト
+	std::unique_ptr <Sprite> sprite = nullptr;
+	// 3Dオブジェクト
+	std::unique_ptr <Object> object[2]{};
+	// パーティクルエミッタ
+	std::unique_ptr <ParticleEmitter> particleEmitter = nullptr;
+
+	Style currentStyle = normal;
+
+};
+
