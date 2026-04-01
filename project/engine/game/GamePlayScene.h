453f9d1212386a17f8a2421147e23896affb7e4f
オノゲンキ <k024G0005@g.neec.ac.jp>
2026-04-01 16:50:38 +0900

    ポーズpng導入

--- project/engine/game/GamePlayScene.h
+++ project/engine/game/GamePlayScene.h
@@ -150,6 +150,12 @@ private:
     std::unique_ptr<Object> object {};
 
     std::unique_ptr<Player> player_ = nullptr;
+
+    std::unique_ptr<Sprite> pause_ = nullptr;
+    std::unique_ptr<Sprite> resume_ = nullptr;
+    std::unique_ptr<Sprite> retry_ = nullptr;
+    std::unique_ptr<Sprite> select_ = nullptr;
+
     Style style_ = Style::normal;
     // リスト
     std::unique_ptr<EnemyManager> enemy_ = nullptr;
