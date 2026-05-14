fdaa855123a3ebf0d44557e84da56350e80d8c25
LC1C_22_moriya_yugo <k024g0016@g.neec.ac.jp>
2026-05-14 16:37:38 +0900

    修正

--- project/engine/game/TitleScene.cpp
+++ project/engine/game/TitleScene.cpp
@@ -59,13 +59,13 @@ void TitleScene::Initialize() {
 		hitEffect[i]->Initialize("hitEffect1", Transform{}, 5, 0.2f);
 	}
 	hitEffect[0]->SetActive("hitEffect1");
-	hitEffect[0]->LoadParticle("Resource/particle/hit_1.csv");
+	hitEffect[0]->LoadParticle("Resource/particle/death_1.csv");
 	hitEffect[1]->SetActive("hitEffect2");
-	hitEffect[1]->LoadParticle("Resource/particle/hit_2.csv");
+	hitEffect[1]->LoadParticle("Resource/particle/death_2.csv");
 	hitEffect[2]->SetActive("hitEffect3");
-	hitEffect[2]->LoadParticle("Resource/particle/hit_3.csv");
+	hitEffect[2]->LoadParticle("Resource/particle/death_3.csv");
 	hitEffect[3]->SetActive("hitEffect4");
-	hitEffect[3]->LoadParticle("Resource/particle/hit_4.csv");
+	hitEffect[3]->LoadParticle("Resource/particle/death_4.csv");
 
 	// パーティクル
 	particleEmitter = std::make_unique<ParticleEmitter>();
