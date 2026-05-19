7ca21700cc2fa04134d327de3a8296f27106b0ab
オノゲンキ <k024G0005@g.neec.ac.jp>
2026-05-19 15:34:09 +0900

    bulletModel導入

--- project/game/enemy/Normal/Bullet/NormalEnemyBullet.cpp
+++ project/game/enemy/Normal/Bullet/NormalEnemyBullet.cpp
@@ -10,7 +10,7 @@ void NormalEnemyBullet::Initialize(Camera* camera, Vector3 Pos)
 
     object_ = std::make_unique<Object>();
     object_->Initialize(camera_);
-    object_->SetModel("plane.obj");
+    object_->SetModel("anpleBullet.obj");
     object_->SetScale({ 0.5f, 0.5f, 0.5f });
     object_->SetRotate(transform_.rotate);
     object_->SetTranslate(transform_.translate);
